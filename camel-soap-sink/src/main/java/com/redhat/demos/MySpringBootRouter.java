a29046383a4019c88cbadbb31951736d0efd1c84
Aly Ibrahim <aly.ibrahim@gmail.com>
2020-05-27 08:00:12 -0500

    SpringBoor Soap Sink Service

---
+++ camel-soap-sink/src/main/java/com/redhat/demos/MySpringBootRouter.java
@@ -0,0 +1,20 @@
+package com.redhat.demos;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+/**
+ * A simple Camel route that triggers from a timer and calls a bean and prints to system out.
+ * <p/>
+ * Use <tt>@Component</tt> to make Camel auto detect this route when starting.
+ */
+@Component
+public class MySpringBootRouter extends RouteBuilder {
+
+    @Override
+    public void configure() {
+        rest("/").post("xml").consumes("application/xml").to("log: XML Train Data Received").to("direct://myservice");
+        from("direct//myservice").to("activemq:traindata-xml?disableReplyTo=true");
+    }
+
+}
